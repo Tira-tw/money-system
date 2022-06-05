37c1f4207a5c66ed15f5348db4588f7d28b246c0
幻月貓 <64715639+Tira-tw@users.noreply.github.com>
2022-06-05 21:38:36 +0800

    Add files via upload

---
+++ README.md
@@ -0,0 +1,47 @@
+# 金錢系統
+> 金錢系統是一個非常方便的機器人 , 擁有7個功能![有一個隱藏功能] <br>
+# 機器人邀請
+> 目前還不公開Bot~ <br>
+# 設定
+> 1.修改config.json的資料 :
+`owner_id` <br>
+`prefix` <br>
+`token`<br> 
+`max`<br>
+`min`<br>
+`new`<br><br>
+    
+owner_id填你的id <br> <br>
+    
+prefix填你的前綴 <br> <br>
+    
+token就填token <br> <br>
+    
+max每次簽到最高獲得數 <br> <br>
+    
+min每次簽到最低獲得數 <br> <br>
+    
+new創建帳號初始金額 <br> <br>
+
+2. pip install discord <br> <br>
+
+3. python bot.py <br> <br>
+
+4. done! <br> <br>
+#版權
+> 開發者已獲得源代碼作者發布許可 , 請勿修改 | 公開 <br>
+源代碼作者 : <br>
+https://github.com/HansHans135 <br>
+
+源代碼 : <br>
+https://github.com/HansHans135/across_guild <br>
+
+Tira-tw開發者 : <br>
+https://github.com/Tira-tw
+
+
+
+
+小提示 : 
+開發者可以使用[prefix]give <ID> <多少錢> 
+
---
+++ bot.py
@@ -0,0 +1,165 @@
+import datetime
+from hashlib import new
+import discord
+import json
+import random
+import os
+
+
+print("--------------------------------------------------------")
+print("開發者已獲得源代碼作者發布許可 , 請勿修改 | 公開")
+print("源代碼作者 :")
+print("https://github.com/HansHans135")
+print("--------------------------------------------------------")
+print("源代碼 :")
+print("https://github.com/HansHans135/across_guild")
+print("--------------------------------------------------------")
+print("開發者Discord :")
+print("天然呆幻月#1314")
+print("--------------------------------------------------------")
+print("開發者Discord支援區 :")
+print("https://discord.gg/3S5BgMTx47")
+print("--------------------------------------------------------")
+
+#設定
+with open ("config.json",mode="r",encoding="utf-8") as filt:
+    data = json.load(filt)
+前輟 = data["prefix"]
+op_id = data["owner_id"]
+TOKEN = data["token"]
+MAX = data["max"]
+MIN = data["min"]
+NEW = data["new"]
+
+
+
+client = discord.Client()
+
+@client.event   
+async def on_ready():
+    print('BOT已上線，Botname：',client.user)
+    status_w = discord.Status.online
+    activity_w = discord.Activity(type=discord.ActivityType.watching, name=f"{前輟}help")
+    await client.change_presence(status= status_w, activity=activity_w)
+
+@client.event
+async def on_message(message):
+#help
+    if message.content == f"{前輟}help":
+        await message.delete()
+        embed = discord.Embed(title="menu", description=f"prefix是{前輟}", color=0x04f108)
+        embed.add_field(name=f"{前輟}help", value="指令功能查詢")
+        embed.add_field(name=f"{前輟}new", value="創建")
+        embed.add_field(name=f"{前輟}sign", value="每日簽到(冷卻時間的計算為每天08:00重製)")
+        embed.add_field(name=f"{前輟}me", value="查看您有多少錢")
+        embed.add_field(name=f"{前輟}money <id>", value="查看別的用戶有多少金錢")
+        embed.add_field(name=f"{前輟}info", value="關於Bot")
+        await message.channel.send(content=None, embed=embed)
+#info
+    if message.content == f"{前輟}info":
+        await message.delete()
+        embed = discord.Embed(title="關於", description=f"prefix是{前輟}", color=0x04f108)
+        embed.add_field(name="每日簽到最高", value=f"{MAX}$")
+        embed.add_field(name="每日簽到最低", value=f"{MIN}$")
+        embed.add_field(name="Bot支援區:", value="https://discord.gg/3S5BgMTx47")
+        embed.add_field(name="關於此Bot:", value="開發者 : 天然呆幻月#1314 <@848164182334898216>")
+        await message.channel.send(content=None, embed=embed)
+#創建帳號
+    if message.content == f"{前輟}new":
+      await message.delete()
+      filepath = f"money/{message.author.id}.json"
+      if os.path.isfile(filepath):
+          await message.channel.send(f"{message.author.mention}您已建立帳號了!")
+      else:
+          with open (f"money/{message.author.id}.json",mode="w",encoding="utf-8") as filt:
+            data = {"last_time":"0","money":NEW,"g":"0"}
+            json.dump(data,filt)
+            money = data['money']
+          await message.channel.send(f"{message.author.mention}帳號建立完成,您目前有`{money}`元")
+
+#簽到
+    if message.content == f"{前輟}sign":
+        await message.delete()
+        await message.channel.send("查詢中 , 請稍等[不會太久]....")
+        filepath = f"money/{message.author.id}.json"
+        if os.path.isfile(filepath):
+            today = datetime.date.today()
+            with open (f"money/{message.author.id}.json",mode="r+",encoding="utf-8") as filt:
+                data = json.load(filt)
+                print(data['money'])
+                last_time = data['last_time']
+            if str(today) == str(last_time):
+                await message.channel.send(f"{message.author.mention}您今天已簽到 , 明日再來")
+            else:
+                with open (f"money/{message.author.id}.json",mode="r",encoding="utf-8") as filt:
+                    data = json.load(filt)
+                    print(data['money'])
+                    upmoney = random.randrange(int(MIN),int(MAX))
+                    newmoney = int(upmoney) + int(data['money'])
+                data['money'] = newmoney
+                with open (f"money/{message.author.id}.json",mode="w",encoding="utf-8") as filt:
+                    json.dump(data,filt)
+                data['last_time'] = str(today)
+                with open (f"money/{message.author.id}.json",mode="w",encoding="utf-8") as filt:
+                    json.dump(data,filt)
+                await message.channel.send(f"{message.author.mention}本日簽到獎勵:`{upmoney}`元\n您目前有`{newmoney}`元")
+        else:
+            await message.channel.send(f"{message.author.mention}您沒有帳號,請建立帳號 , [`{前輟}!new`創建一個]")
+#查詢
+    if message.content == f"{前輟}me":
+        with open (f"money/{message.author.id}.json",mode="r",encoding="utf-8") as filt:
+            data = json.load(filt)
+        await message.channel.send(f"{message.author.mention}您目前有`{data['money']}`元")
+
+    if message.content.startswith(f'{前輟}money'):
+      await message.delete()
+      tmp = message.content.split(" ",2)
+      if len(tmp) == 1:
+        await message.channel.send("未提供用戶ID")
+      else:
+        with open (f"money/{tmp[1]}.json",mode="r",encoding="utf-8") as filt:
+            data = json.load(filt)
+        await message.channel.send(f"{message.author.mention}您好 , 您查詢的使用者<@{tmp[1]}> `{tmp[1]}`他現在有`{data['money']}`元")
+#給錢 , 只限於開發者
+    if message.content.startswith(f'{前輟}give'):
+        if message.author.id == int(op_id):
+          await message.delete()
+          tmp = message.content.split(" ",2)
+          崁入一 = tmp[1]
+          tmp = message.content.split(f"{崁入一} ",2)
+          崁入二 = tmp[1]
+          #亨哥0126 | 天然呆幻月#1314
+          if len(tmp) == 1:
+            await message.channel.send(f"{前輟}give id 錢")
+          else:
+              filepath = f"money/{崁入一}.json"
+              if os.path.isfile(filepath):
+                with open (f"money/{崁入一}.json",mode="r",encoding="utf-8") as filt:
+                    data = json.load(filt)
+                data['money'] = int(崁入二)
+                with open (f"money/{崁入一}.json",mode="w",encoding="utf-8") as filt:
+                    json.dump(data,filt)
+                with open (f"money/{崁入一}.json",mode="r",encoding="utf-8") as filt:
+                    data = json.load(filt)
+                await message.channel.send(f"已將id:`{崁入一}`設為`{data['money']}`元")
+              else:
+                await message.channel.send(f"找不到關於id:`{崁入一}`的帳號")
+        else:
+            await message.channel.send(f"{message.author.mention}你沒有權限")
+
+#查詢資料
+    if message.content.startswith(f'{前輟}money'):
+      await message.delete()
+      tmp = message.content.split(" ",2)
+      if len(tmp) == 1:
+        await message.channel.send("未提供用戶ID")
+      else:
+        filepath = f"money/{tmp[1]}.json"
+        if os.path.isfile(filepath):
+            with open (f"money/{tmp[1]}.json",mode="r",encoding="utf-8") as filt:
+                data = json.load(filt)
+            await message.channel.send(f"{message.author.mention}您好 , 您查詢的使用者<@{tmp[1]}>`{tmp[1]}`他現在有`{data['money']}`元")
+        else:
+            await message.channel.send("未找到這筆資料")
+
+client.run(TOKEN)
